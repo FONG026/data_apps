4701107d5992f300dfe42dbb8de1c392ba092fcd
FONG026 <93886335+FONG026@users.noreply.github.com>
2024-03-14 13:49:03 +0100

    Update payments_analysis.py

--- apps/pages/payments_analysis.py
+++ apps/pages/payments_analysis.py
@@ -42,7 +42,7 @@ st.plotly_chart(fig, use_container_width=True)
 st.header('Orders status')
 result = calculator.orders_status()
 orders_status = result['Orders status']
-print(order_status)
+order_status
 #fig =px.bar(x=orders_status.index, y=orders_status, title='Orders Status', labels={'y': 'Percentage'})
 #fig=px.pie(values=orders_status.index, names=orders_status.index, title='Orders Status')
 #st.plotly_chart(fig, use_container_width=True)
